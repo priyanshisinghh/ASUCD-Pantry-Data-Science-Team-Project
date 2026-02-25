271b3f46068261131af518a6b73e6be395c08a5f
priyanshi singh <88902365+priyanshisinghh@users.noreply.github.com>
2026-02-25 01:02:16 -0800

    Update jaxon.py

---
+++ pages/jaxon.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.header("Jaxon's Findings")
+st.write("""
+Insert all your findings here in whatever way you want. 
+         Make sure you show and explain your findings in a "storytelling"ish way that someone with a non-technical background would understand. 
+""")
