240bfe302eae5ee7b238be103bf9ed39aba938f5
priyanshi singh <88902365+priyanshisinghh@users.noreply.github.com>
2026-03-02 16:53:50 -0800

    Delete pages/.ipynb_checkpoints directory

--- pages/.ipynb_checkpoints/pri-checkpoint.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.header("Chi-Squared Testing")
-st.write(""" Is there an association between "How often patrons find what they need" and "Types of items requested" (fruits, snacks, protein)""")
