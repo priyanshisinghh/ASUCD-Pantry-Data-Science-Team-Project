59961dae1ec9fd725e339db251c60467659f8e6a
risitha <74447401+risithabellignur@users.noreply.github.com>
2026-04-17 03:24:24 -0700

    Update risitha.py

--- pages/risitha.py
+++ pages/risitha.py
@@ -213,12 +213,12 @@ st.subheader("Interpretation")
 
 if p < 0.05:
     st.error("""
-Based on the visualization and chi-square p-value result being < 0.5, 
+Based on the visualization and p-value result being < 0.5, 
 there exists a significant relationship between the selected group and difficulty finding produce.
 """)
 else:
     st.success("""
-Based on the visualization and chi-square p-value result being > 0.5, 
+Based on the visualization and p-value result being > 0.5, 
 there is no significant relationship between the selected group and difficulty finding produce.
 
 """)
