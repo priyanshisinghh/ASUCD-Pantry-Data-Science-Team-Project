4b83f9a7a468bec4483150aea6af97ce141f431d
priyanshi singh <88902365+priyanshisinghh@users.noreply.github.com>
2026-04-18 01:42:30 -0700

    Update pri.py

--- pages/pri.py
+++ pages/pri.py
@@ -9,7 +9,7 @@ def get_base64(file_path):
     with open(file_path, "rb") as f:
         return base64.b64encode(f.read()).decode()
 
-img = get_base64("static/pri-bg.png")
+img = get_base64("pages/images/pri-bg.png")
 
 st.markdown(f"""
     <style>
